a315a4ca6382ccdb810425eab0c96de942a33d3e
Rendra Suroso <rs@cogsci.bandungfe.net>
2017-04-12 17:20:28 +0700

    Tweak build configs.

--- src/MySQL.php
+++ src/MySQL.php
@@ -6,6 +6,14 @@ namespace BFITech\ZapStore;
 
 class MySQL extends SQL {
 
+	/**
+	 * Constructor.
+	 *
+	 * @param array $params SQL connection dict exactly the same with
+	 *     that in the parent class except that 'dbtype' key can be
+	 *     omitted.
+	 * @param Logger $logger Instance of BFITech\\ZapCore\\Logger.
+	 */
 	public function __construct($params, $logger=null) {
 		$params['dbtype'] = 'mysql';
 		parent::__construct($params, $logger);
